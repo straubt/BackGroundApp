37d46b30541c014b8869f3978d7686b2d93ba848
STRAUBJACQUIN <titouan.straub@etu.univ-smb.fr>
2024-01-23 13:46:35 +0100

    Initial Commit

---
+++ BackgroundApp/BackgroundAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BackgroundAppApp.swift
+//  BackgroundApp
+//
+//  Created by Titouan STRAUB on 1/23/24.
+//
+
+import SwiftUI
+
+@main
+struct BackgroundAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
